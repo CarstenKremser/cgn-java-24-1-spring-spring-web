7d2ec0426ddbd2fc0a8d84fe4e78937460413860
Carsten Kremser <github@carstenkremser.de>
2024-04-02 15:41:53 +0200

    Add Bonus: hello someone

--- src/main/java/de/carstenkremser/neuefische/cgnjava241springspringweb/HelloWorldController.java
+++ src/main/java/de/carstenkremser/neuefische/cgnjava241springspringweb/HelloWorldController.java
@@ -1,6 +1,7 @@
 package de.carstenkremser.neuefische.cgnjava241springspringweb;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,9 @@ class HelloWorldController {
     @GetMapping(path = "/hello")
     public String hello() {
         return "Hello, World!";
+    }
+    @GetMapping(path = "/hello/{name}")
+    public String hello(@PathVariable String name) {
+        return "Hello, "+name+"!";
     }
 }
