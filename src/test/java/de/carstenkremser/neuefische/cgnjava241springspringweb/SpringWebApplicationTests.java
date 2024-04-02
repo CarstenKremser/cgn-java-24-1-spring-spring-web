68b880bf0ef80ee0ac9e47d67b335e7b01995e9a
Carsten Kremser <github@carstenkremser.de>
2024-04-02 15:36:50 +0200

    Initial Commit

---
+++ src/test/java/de/carstenkremser/neuefische/cgnjava241springspringweb/SpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package de.carstenkremser.neuefische.cgnjava241springspringweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
